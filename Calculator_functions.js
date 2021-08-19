a2d8527fe9c9831eaae7449989a07db94ed5ebbf
Rodrigo Campos <rodrigo.cdpadua@gmail.com>
2021-08-19 14:56:39 -0300

    basic functions

---
+++ Calculator_functions.js
@@ -0,0 +1,19 @@
+function sum(num1, num2){
+  return num1 + num2;
+}
+
+function subtract(num1, num2){
+  return num1 - num2;
+}
+
+function multiplis(num1, num2){
+  return num1 * num2;
+}
+
+function divide(num1, num2){
+  return num1 / num2;
+}
+
+
+
+console.log(sum(1,2));
